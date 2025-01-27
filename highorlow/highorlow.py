3bb9d8df59f5f15a451d72d8607668baf3e04bcd
FireF4ng <daniel.centov@gmail.com>
2025-01-27 11:50:56 +0100

    code reading

--- highorlow/highorlow.py
+++ highorlow/highorlow.py
@@ -2,36 +2,36 @@ from random import randint
 
 class Game:
     def __init__(self):
-        self.MIN_NUMBER = 1
-        self.MAX_NUMBER = 100
-        self.MAX_GUESSES = 10
-        self.randnumber = randint(self.MIN_NUMBER, self.MAX_NUMBER)
-        self.numberofguesses = 0
-        self.allguesses = []
-        self.correctguess = False
+        self.min_number = 1
+        self.max_number = 100
+        self.max_guesses = 10
+        self.rand_number = randint(self.min_number, self.max_number)
+        self.number_of_guesses = 0
+        self.all_guesses = []
+        self.correct_guess = False
     
     def play(self):
         print("Welcome to the Higher or Lower Game")
-        print("Your goal is to guess the generated number ("+str(self.MIN_NUMBER) +
-              "-"+str(self.MAX_NUMBER)+") within "+str(self.MAX_GUESSES)+" tries")
+        print("Your goal is to guess the generated number ("+str(self.min_number) +
+              "-"+str(self.max_number)+") within "+str(self.max_guesses)+" tries")
         print("Good luck !!!")
-        while self.numberofguesses < self.MAX_GUESSES:
-            print("You have " + str(self.MAX_GUESSES-self.numberofguesses) +
+        while self.number_of_guesses < self.max_guesses:
+            print("You have " + str(self.max_guesses-self.number_of_guesses) +
                   " guesses left")
             guess = int(input("Enter your guess: "))
-            self.numberofguesses += 1
-            self.allguesses.append(guess)
-            if self.randnumber > guess:
+            self.number_of_guesses += 1
+            self.all_guesses.append(guess)
+            if self.rand_number > guess:
                 print("The number is higher")
-            elif self.randnumber < guess:
+            elif self.rand_number < guess:
                 print("The number is lower")
             else:
                 print("Congratulations, you have guessed the number")
-                self.correctguess = True
+                self.correct_guess = True
                 break
             print("--------------------------------")
-        print("The number was: " + str(self.randnumber))
-        if not self.correctguess:
+        print("The number was: " + str(self.rand_number))
+        if not self.correct_guess:
             print("You have run out of guesses, better luck next time")
     
 
